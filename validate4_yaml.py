4a4dc59efa55c412ce5a0bea4bada3df8fde7824
Mohammed Arif <arif29mohammed97@gmail.com>
2023-10-21 22:00:42 +0530

    testing for yaml

---
+++ validate4_yaml.py
@@ -0,0 +1,15 @@
+import yaml
+import sys
+
+def validate_yaml(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            yaml.safe_load(file)
+        print(f"YAML validation successful for {file_path}")
+        return 0
+    except Exception as e:
+        print(f"YAML validation failed for {file_path}: {str(e)}")
+        return 1
+    
+
+validate_yaml(".\yaml_file.yaml")
